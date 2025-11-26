d69d30b8fafabd85262555ca2e9e3c1824375597
Algimantas <125826902+AlgimantasVz@users.noreply.github.com>
2025-11-26 03:16:02 +0200

    added classes

---
+++ Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public enum ClassType { None, Pawn, King, Queen, Rook, Bishop, Knight };
+
+public class PlayerClass : MonoBehaviour
+{
+    public ClassType playerClass;
+    public Transform rightHand;
+    public GameObject pawnWeaponPrefab;
+
+    private GameObject currentWeapon;
+
+    public void AssignClass(ClassType assignedClass)
+    {
+        playerClass = assignedClass;
+        ApplyLoadout();
+    }
+
+    private void ApplyLoadout()
+    {
+        if (currentWeapon != null) Destroy(currentWeapon);
+
+        switch (playerClass)
+        {
+            case ClassType.None: default: break;
+
+            case ClassType.Pawn: 
+                currentWeapon = Instantiate(pawnWeaponPrefab, rightHand);
+                currentWeapon.transform.localPosition = Vector3.zero;
+                currentWeapon.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+                break;
+
+            case ClassType.King: break;
+
+            case ClassType.Queen: break;
+
+            case ClassType.Rook: break;
+
+            case ClassType.Bishop: break;
+
+            case ClassType.Knight: break;
+
+
+        }
+    }
+}
+
+
