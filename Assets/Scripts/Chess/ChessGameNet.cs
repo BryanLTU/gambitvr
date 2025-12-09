2bcb975abcd31ef152efd9106fd3d70b83107472
BryaN <60313619+BryanLTU@users.noreply.github.com>
2025-12-09 23:41:50 +0200

    Update ChessGameNet.cs

--- Assets/Scripts/Chess/ChessGameNet.cs
+++ Assets/Scripts/Chess/ChessGameNet.cs
@@ -113,9 +113,11 @@ public class ChessGameNet : NetworkBehaviour
             return;
 
         var targetSquare = _game.GetSquare(file, rank);
+        Utils.Log($"targetSquare {targetSquare == null}");
         if (targetSquare == null) return;
 
         bool moved = _game.TryMove(piece, targetSquare);
+        Utils.Log($"moved {!moved} currentSqaure {piece.currentSquare != null}");
         if (!moved && piece.currentSquare != null)
         {
             piece.SetSquare(piece.currentSquare);
