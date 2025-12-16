a904ee21422adfa1784fd4e4534cd8eb11bbcfa7
BryaN <60313619+BryanLTU@users.noreply.github.com>
2025-12-16 20:37:23 +0200

    Remove debug log

--- Assets/Scripts/Chess/ChessGameNet.cs
+++ Assets/Scripts/Chess/ChessGameNet.cs
@@ -199,7 +199,6 @@ public class ChessGameNet : NetworkBehaviour
         _duelActive = true;
 
         _game.BuildSnapshotByPieceId(out var ids, out var files, out var ranks);
-        Debug.Log($"Snapshot ids={ids.Length} fies={files.Length} ranks={ranks.Length}");
         FPSArenaManager.Instance.SnapArenaToBoardServerRpc(ids, files, ranks);
 
         FPSArenaManager.Instance.StartDuelRpc();
